b5eef2f395e835a36f82d18ac7a3f5840064c484
Damian Pasterz <damian.pasterz@gmail.com>
2022-07-07 16:44:12 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 
-import './App.css';
+import React, { useState, useRef } from "react";
+import styled from "styled-components";
+import "./style/App.css"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <MainForm>
+      <Label>
+        <h3> Podaj link lub id filmu: </h3>
+        <ClearAndImport>
+          <SearchBar>
+            {" "}
+            <input ref={inputRef} className="link" type="text" />{" "}
+            <button onClick={addFilm} className="roundedSearch">
+              <SearchIcon />
+            </button>
+          </SearchBar>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
-
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Button variant="outline-danger" onClick={clearFilmList}>
+            Wyczyść listę
+          </Button>{" "}
+        </ClearAndImport>
+      </Label>
+    </MainForm>
+  )
 }
+const MainForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  align-items: center;
+  gap: 10px;
+  Form {
+    align-self: flex-end;
+  }
+`;
+const Label = styled.label`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SearchBar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
+const ClearAndImport = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  Button {
+    align-self: flex-start;
+    margin: 5px 0px;
+  }
+`;
 
 export default App;
